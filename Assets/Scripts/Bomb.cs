64541f82afe6cdb158905fa80114b0cea9d9f64e
Olivier3456 <olivier.larrey@gmail.com>
2023-01-12 10:39:31 +0100

    Bomb class, object + drag & drop class for the bomb

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DragObject : MonoBehaviour     // Permet de faire un drag and drop avec la souris sur l'objet.
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);    
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint);
+        transform.position = cursorPosition;
+    }
+}
