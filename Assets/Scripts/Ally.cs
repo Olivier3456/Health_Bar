cc60e37c09129cb289506c6d9458913d1aeb70f3
Olivier3456 <olivier.larrey@gmail.com>
2023-01-12 11:54:42 +0100

    Ally follow Player

--- Assets/Scripts/Ally.cs
+++ Assets/Scripts/Ally.cs
@@ -11,14 +11,28 @@ public class Ally : MonoBehaviour
         get { return health; }
     }
 
-    
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float followDistance = 2f;
+    [SerializeField] private float allySpeed = 0.5f;
 
 
-    void Start()
+    private void Start()
     {
         health = initialHealth;
         
     }
+
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) >= followDistance)
+        {
+            transform.position += (player.position - transform.position).normalized * allySpeed * Time.deltaTime;
+        }
+    }
+
+
     public void ChangeHP(int hpToRemoveOrAdd)
     {
         health -= hpToRemoveOrAdd;        
